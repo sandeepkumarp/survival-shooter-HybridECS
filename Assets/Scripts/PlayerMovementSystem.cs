377e36175d93af50277a9d88b2c88d38104e8077
Sandeep <sandeepkumarp@users.noreply.github.com>
2018-09-07 09:12:55 +0530

    Player Movement

---
+++ Assets/Scripts/PlayerMovementSystem.cs
@@ -0,0 +1,23 @@
+﻿using Unity.Entities;
+using UnityEngine;
+
+public class PlayerMovementSystem : ComponentSystem
+{
+    private struct Filter
+    {
+        public Rigidbody Rigidbody;
+        public InputComponent InputComponent;
+    }
+    protected override void OnUpdate()
+    {
+        float deltaTime = Time.deltaTime;
+
+        foreach (Filter entity in GetEntities<Filter>())
+        {
+            Vector3 moveVector = new Vector3(entity.InputComponent.Horizontal, 0, entity.InputComponent.Vertical);
+            Vector3 movePosition = entity.Rigidbody.position + moveVector.normalized * 3 * deltaTime;
+
+            entity.Rigidbody.MovePosition(movePosition);
+        }
+    }
+}
