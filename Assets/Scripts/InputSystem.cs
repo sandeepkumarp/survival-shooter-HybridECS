c8ed99690f75e795910e32dc21d539a4a2eb3729
Sandeep <sandeepkumarp@users.noreply.github.com>
2018-09-09 09:36:49 +0530

    Input System

---
+++ Assets/Scripts/InputSystem.cs
@@ -0,0 +1,28 @@
+﻿using Unity.Entities;
+using UnityEngine;
+
+public class InputSystem : ComponentSystem
+{
+    private struct Data
+    {
+        //reserved name: number of entities unity find with this struct.
+        //need to be readonly or else will result in memory leak.
+        public readonly int Length;
+
+        //Tells Unity to scan the scene for Entities that have an InputComponent
+        public ComponentArray<InputComponent> InputComponent;
+    }
+
+    [Inject] private Data _data;
+
+    protected override void OnUpdate()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        for (int i = 0; i < _data.Length; i++)
+        {
+            _data.InputComponent[i].Horizontal = horizontal;
+            _data.InputComponent[i].Vertical = vertical;
+        }
+    }
+}
